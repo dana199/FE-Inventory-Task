e930c389d1aa1e2bb7b349c769b3b83f1dd8d8d9
Dana Hmooda <dhmooda@restaurant365.com>
2025-08-02 05:51:07 +0300

    Initial commit for InventoryManager task

---
+++ src/main.ts
@@ -0,0 +1,23 @@
+import { InsufficientStockError, ItemNotFoundError } from "./errors/InventoryErrors";
+import { InventoryManager } from "./managers/InventoryManager";
+
+async function initializeInventory() {
+    // This function can be used to initialize the inventory with some default items if needed  
+    const manager = await InventoryManager.create();
+    console.log("Inventory Manager initialized:", manager);
+
+    try {
+        await manager.updateQuantity('2', +5);
+        const afterPlus = await manager.getItem('2');
+        console.log('After +5:', afterPlus.quantity); 
+        await manager.updateQuantity('2', -11);
+    } catch (error) {
+        if (error instanceof ItemNotFoundError || error instanceof InsufficientStockError) {
+            console.error(' Error:', error.message);
+        } else {
+            console.error('Unexpected error:', error);
+        }
+    }
+}
+
+initializeInventory();
